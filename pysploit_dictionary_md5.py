f8c651814f7f0cfd6062914c6547e048d5ec3b79
MohammadIsCoding1 <80381539+MohammadIsCoding1@users.noreply.github.com>
2021-05-27 09:38:10 -0500

    Create pysploit_dictionary_md5.py

---
+++ pysploit_dictionary_md5.py
@@ -0,0 +1,19 @@
+import hashlib
+
+user_hash_dict={}
+
+with open('1000000Password.txt', 'r') as f:
+    common_passwords=f.read().splitlines()
+
+with open('username_hashes.txt', 'r') as f:
+    text=f.read().splitlines()
+    for user_hash in text:
+        username=user_hash.split(":")[0]
+        hash=user_hash.split(":")[1]
+        user_hash_dict[username]=hash
+
+for password in common_passwords:
+    hashed_password=hashlib.md5(password.encode('utf-8')).hexdigest()
+    for username, hash in user_hash_dict.items():
+        if hashed_password==hash:
+            print(f'*[HASH FOUND]\n{username}:{password}')
